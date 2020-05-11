68edc213600715112ff1a6beaf0d95dab03bc208
Vaibhav Vyas <vaibhavvyas98@outlook.com>
2020-05-11 10:06:50 +0530

    Commit staged

---
+++ WebApi/Controllers/FileUploadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Web.Http.Cors;
+
+namespace WebApi.Controllers
+{
+    [EnableCors(origins: "http://localhost:44315/Home/Index.html", headers: "*", methods: "*")]
+    public class FileUploadController : Controller
+    {
+        [HttpPost]
+        public void UploadFile()
+        {
+            if (HttpContext.Current.Request.Files.AllKeys.Any())
+            {
+                // Get the uploaded image from the Files collection
+                var httpPostedFile = HttpContext.Current.Request.Files["UploadedImage"];
+
+                if (httpPostedFile != null)
+                {
+                    // Validate the uploaded image(optional)
+
+                    // Get the complete file path
+                    var fileSavePath = Path.Combine(HttpContext.Current.Server.MapPath("~/UploadedFiles"), httpPostedFile.FileName);
+
+                    // Save the uploaded file to "UploadedFiles" folder
+                    httpPostedFile.SaveAs(fileSavePath);
+                }
+            }
+        }
+
+        [HttpGet]
+
+        public string GetPath()
+        {
+            string path = File.ReadAllText("Paths.txt");
+            return path;
+        }
+    }
+}
---
+++ WebApi/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+
+
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                     name: "DefaultApi",
+                     routeTemplate: "api/{controller}/{action}/{id}",
+                     defaults: new { id = RouteParameter.Optional }
+                     );
+            });
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
